9b864c757c614fe02b1561c77ecd6648fcd7888c
Polacoj <polacoj@hotmail.com>
2024-04-29 17:30:32 -0300

    Poniendo algo

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,7 +1,30 @@
-let num1
-let num2
-flag = true
+var choice = 0;
+let num1 = 0;
+let num2 = 0;
+let res = Number;
 
-while (!flag){
-    
+switch (console.log(prompt("ingrese eleccion : ", choice))) {
+  case 1:
+    console.log(prompt("numero1: ", num1));
+    console.log(prompt("numero 2: ", num2));
+    res = num1 + num2;
+    console.log("resultado", res);
+    break;
+  // case 1:
+  //   output += "Cuál ";
+  //   output += "Es ";
+  // case 2:
+  //   output += "Tu ";
+  // case 3:
+  //   output += "Nombre";
+  // case 4:
+  //   output += "?";
+  //   console.log(output);
+  //   break;
+  // case 5:
+  //   output += "!";
+  //   console.log(output);
+  //   break;
+  // default:
+  //   console.log("Por favor, selecciona un valor del 1 al 6.");
 }
